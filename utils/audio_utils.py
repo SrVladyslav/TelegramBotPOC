42e3c8b28f5f589cb2bb9786447f642ae4d62f7c
Vladyslav <vlama@inf.upv.es>
2024-05-02 02:06:12 +0200

    New resampling method implemented due to old one bad functionality

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,5 +1,6 @@
 from data.database_handler import DatabaseHandler
 import soundfile as sf
+import librosa
 import io
 import os 
 
@@ -11,10 +12,18 @@ class AudioUtils:
         self._dh = DatabaseHandler()                                       
 
     def createNewFolder(self, folder_path:str):
+        """Create a new folder at the specified path if it does not already exist.
+
+        Args:
+            folder_path (str): Path of the folder to be created.
+
+        Raises:
+            OSError: If an error occurs during folder creation.
+        """
         try:
             os.makedirs(folder_path, exist_ok=True)
         except OSError as e:
-            # Add Loggs here
+            # Log: Error on folder creation.
             print(f'Error on folder creation {folder_path}: {e}')
 
     async def processAudio(self, audio_data:bytearray, user_id:int):
@@ -24,29 +33,30 @@ class AudioUtils:
             audio_data (bytearray): Raw audio data to be processed and saved.
             user_id (int): Unique Telegram identifier of the user associated with the audio.
         """
+        if self._new_sampling_rate <= 0:
+            return None
+        
+        # Load the data and resample to 16KHz rate.
+        # NOTE: Use 'soxr_hq' to minimaze the Aliasing effect
+        audio_wave, samplerate = librosa.load(io.BytesIO(audio_data), sr=self._new_sampling_rate, res_type='soxr_hq')
+    
         # Create new Audio item in the DB and return the new User's audio name
         user_audio_filename = self._dh.postUserAudio(user_id)
 
         # The Audio recording format is: uid -> [audio_message_0, audio_message_1, ..., audio_message_N]
         usr_audio_folder_path = f'{self._BASE_DIR}/data/audio_data/{user_id}/'
+        # Create new folder if it does not already exist.
         self.createNewFolder(usr_audio_folder_path)
-
-        # NOTE: I assume that there won't be any problems on file creation, otherwise
-        # some fail-checking implementations will be needed since we already created 
-        # a DB record for this audio.
         user_audio_path = f'{usr_audio_folder_path}/{user_audio_filename}.wav'
-
-        # NOTE: In the prod case it would be good to also preprocess the audio,
-        # for example, apply the Anti-Aliasing filter, this could be done with Scipy.
-        data, samplerate = sf.read(io.BytesIO(audio_data))
-        # Store an audio with a sampling rate of 16kHz
-        sf.write(user_audio_path, data, self._new_sampling_rate)
-
+        # Store the audio in the corresponding PATH, with a sampling rate of 16kHz and in .WAV format
+        sf.write(user_audio_path, audio_wave, self._new_sampling_rate, subtype='PCM_24')
+        
+        # NOTE: Descomment only for Dev purposes
         # self.get_sample_rate(user_audio_path) # Check      
 
     # DEVELOPMENT PURPOSES ======================================================================
     def get_sample_rate(self, path:str):
         """Check function to obtain the sample rate from audio of the given path"""
         data, samplerate = sf.read(path)
-        print("New RATE: ",samplerate)
+        print("New Sampling RATE: ",samplerate)
 
